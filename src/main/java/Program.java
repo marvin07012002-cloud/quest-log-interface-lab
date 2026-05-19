b76392a195e6f3e24ed747c09dfe2fcf567edc77
marbin701 <marvin07012002@gmail.com>
2026-05-19 10:43:29 -0400

    main class working

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,2 +1,10 @@
 public class Program {
+
+    public static void main(String[] args) {
+
+        MonsterDefeat monsterDefeat = new MonsterDefeat("Toxic Roach",99,true);
+
+        System.out.println(monsterDefeat.getSummary());
+
+    }
 }
