97252e582988dec7d9954710aae80e5e6b5d545a
marbin701 <marvin07012002@gmail.com>
2026-05-19 13:05:11 -0400

    working on QuestLog class right now

---
+++ src/main/java/QuestLog.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestLog {
+    private List<ScoreEvent> events;
+
+    public QuestLog() {
+        events = new ArrayList<>();
+    }
+
+    public void addEvent(ScoreEvent event){
+
+    }
+
+    public int getTotalScore(){
+        return 0;
+    }
+
+    public ScoreEvent getHighestScoringEvent(){
+
+    }
+
+    public ScoreEvent getLowestScoringEvent(){
+
+    }
+
+    public void printSummary(){
+
+    }
+}
